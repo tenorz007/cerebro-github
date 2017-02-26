efcec26dc038cd07b509f1c1738c68f8f4d924d5
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-02-26 13:40:07 +0100

    Added input parser
    * Translates user input to query

---
+++ src/input.js
@@ -0,0 +1,116 @@
+const operatorMapper = {
+    'written in ': 'language:',
+    'in ': 'location:',
+    'with ': ':',
+    'having ': ':',
+    'more than ': '>',
+    'less than ': '<',
+    'at least ': '>=',
+    'at most ': '<=',
+    'trending users': '',
+    'trending repos': '',
+    'this week': 'weekly',
+    'this month': 'monthly',
+    'between ': '',
+    'in': '',
+    'using': '',
+    ' and ': '..'
+};
+const operands = ['stars', 'forks', 'repos', 'followers', 'topics', 'size'];
+const periods = ['today', 'weekly', 'monthly'];
+
+const parseInput = (term) => {
+    let input = {type: '', path: '', query: {}};
+    let query;
+
+    term = term.toLowerCase();
+    query = replaceAll(term, operatorMapper);
+    query = buildQuery(query);
+
+    if (/trending users/.test(term)) {
+        input = parseTrendingUsers(query, input);
+    } else if (/trending/.test(term)) {
+        input = parseTrending(query, input);
+    } else if (term.split(' ')[0] === 'user' || term.split(' ')[1] === 'user') {
+        input = parseUsers(query, input);
+    } else {
+        input = parseRepos(query, input);
+    }
+
+    return input;
+};
+
+function replaceAll(term, values) {
+    let matches = Object.keys(values).join('|');
+    let regex = new RegExp(`(${matches})`, 'g');
+
+    return term.replace(regex, function (a, b) {
+        let r = values[b];
+        return typeof r === 'string' ? r : a;
+    });
+}
+
+function buildQuery(term) {
+    let query = [];
+
+    term.split(' ').forEach(function(element, i) {
+        if (operands.indexOf(element) >= 0) {
+            query[query.length - 1] = element + query[query.length - 1];
+        } else if (element !== '' && element !== 'user' && element !== 'repo') {
+            query.push(element);
+        }
+    })
+
+    return query;
+}
+
+function getPath(query, input) {
+    query.forEach(function(element) {
+        if (periods.indexOf(element) >= 0) {
+            input.query = {since : element};
+        } else {
+            input.path = element;
+        }
+    });
+
+    return input;
+}
+
+function parseTrending(query, input) {
+    input = getPath(query, input);
+    input.path = 'trending/' + input.path;
+    input.type = 'trending';
+
+    return input;
+}
+
+function parseTrendingUsers(query, input) {
+    input = getPath(query, input);
+    input.type = 'trending-users';
+
+    return input;
+}
+
+function parseUsers(query, input) {
+    input.type = 'users';
+    input.query = {
+        type: 'Users',
+        q: query.join(' ')
+    };
+
+    return input;
+}
+
+function parseRepos(query, input) {
+    input.type = 'repos';
+    input.query = {
+        type: 'Repositories',
+        q: query.join(' ')
+    };
+
+    return input;
+}
+
+module.exports = {
+    parseInput
+};
