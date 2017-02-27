cf113dcc10cd232c35b4f13212154e7a8c4b2d2d
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-02-27 13:32:29 +0100

    Updated 'preview' to retrieve data and use components

--- src/preview.js
+++ src/preview.js
@@ -1,6 +1,10 @@
 const React = require('react');
-const { parseInput } = require('./input');
+const parseInput = require('./input');
+const Spinner = require('react-spinkit');
 const { searchGithub } = require('./search');
+const Repo = require('./repo');
+const TrendingUser = require('./trending-user');
+const User = require('./user');
 
 class Preview extends React.Component {
     constructor(props) {
@@ -11,8 +15,8 @@ class Preview extends React.Component {
             repos: [],
             users: [],
             error: {
-                type: '',
-                message: ''
+                type: null,
+                message: null
             }
         }
     }
@@ -21,17 +25,88 @@ class Preview extends React.Component {
         let term = parseInput(this.props.term);
 
         searchGithub(term)
-            .then(res => this.setState({ [term.type]: res }))
-            .then(err => this.setState({ error: {type: term.type, message: err}}));
+            .then(res => this.setState({
+                [term.type]: res,
+                error: {message: null, type: null}
+            }))
+            // .catch(err => this.setState({ error: {type: term.type, message: err}}));
+            .catch(err => console.log(err));
     }
 
+    renderTrendingRepos() {
+        const { trending } = this.state;
+
+        return trending.map((repo, idx) => (
+            <Repo key={idx} repo={repo}  />
+        ))
+    }
+
+    renderTrendingUsers() {
+        const { trendingUsers } = this.state;
+
+        return trendingUsers.map((user, idx) => (
+            <TrendingUser key={idx} user={user}  />
+        ))
+    }
+
+    renderRepos() {
+        const { repos } = this.state;
+
+        return repos.map((repo, idx) => (
+            <Repo key={idx} repo={repo}  />
+        ))
+    }
+
+    renderUsers() {
+        const { users } = this.state;
+
+        return users.map((user, idx) => (
+            <User key={idx} user={user}  />
+        ))
+    }
+
+
     render() {
+        const { trending, trendingUsers, repos, users, error } = this.state;
+
+        if (error.message) {
+            return (
+                <Error message={error.message} type={error.type} />
+            );
+        }
+
+        if (! trending.length && ! trendingUsers.length && ! users.length && ! repos.length) {
+            return <Spinner spinnerName='wave' noFadeIn />
+        }
+
+
+        if (trending.length > 0) {
+            return (
+                <div>
+                    { this.renderTrendingRepos() }
+                </div>
+            )
+        }
+
+        if (trendingUsers.length > 0) {
+            return (
+                <div>
+                    { this.renderTrendingUsers() }
+                </div>
+            )
+        }
+
+        if (users.length > 0) {
+            return (
+                <div>
+                    { this.renderUsers() }
+                </div>
+            )
+        }
+
         return (
             <div>
-                <pre>{ this.state.trending }</pre>
-                <pre>{ this.state.trendingUsers }</pre>
-                <pre>{ this.state.repos }</pre>
-                <pre>{ this.state.users }</pre>
+                { this.renderRepos() }
             </div>
         );
     }
@@ -39,6 +114,6 @@ class Preview extends React.Component {
 
 Preview.propTypes = {
     term: React.PropTypes.string.isRequired
-}
+};
 
 module.exports = Preview;
