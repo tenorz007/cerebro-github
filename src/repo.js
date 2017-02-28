a3755883ed3dc867b297dd51cb19a14e29f6cecd
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-02-28 14:45:31 +0100

    Updated repo component styling

--- src/repo.js
+++ src/repo.js
@@ -4,21 +4,70 @@ class Repo extends React.Component {
     render() {
         const { name, url, description, language, language_color, stars, stars_in_period, forks, updated } = this.props.repo;
 
+        let repoLanguage;
+        let repoStars;
+        let repoForks;
+        let repoStarsInPeriod;
+        let repoUpdated;
+
+        if (language) {
+            repoLanguage = <small className="level-item">
+                                <span className="tag link-tag icon is-small"><i className="fa fa-circle-thin" color={language_color}></i>{language}</span>
+                            </small>
+        }
+
+        if (stars) {
+            repoStars = <small className="level-item">
+                            <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stars}</span>
+                        </small>
+        }
+
+        if (forks) {
+            repoForks = <small className="level-item">
+                            <span className="tag link-tag icon is-small"><i className="fa fa-code-fork"></i>{forks}</span>
+                        </small>
+        }
+
+        if (stars_in_period) {
+            repoStarsInPeriod = <small className="level-item">
+                                    <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stars_in_period}</span>
+                                </small>
+        }
+
+        if (updated) {
+            repoUpdated = <small className="level-item">
+                                <span className="tag link-tag icon is-small"><i className="fa fa-pencil-square-o"></i>{updated}</span>
+                            </small>
+        }
+
         return (
-            <div>
-                <article>
-                    <div>
-                        <h2>{name}</h2>
-                        <p>{description}</p>
-                        <div>
-                            <span>Language: {language}</span>
-                            <span>Stars: {stars}</span>
-                            <span>Forks: {forks}</span>
-                            <span>{stars_in_period}</span>
-                            <span>{updated}</span>
-                        </div>
+            <div className="card">
+                <div className="card-content">
+                    <div className="media-content">
+                        <p className="title is-5"><i className="fa fa-storage"></i><strong>{name}</strong></p>
                     </div>
-                </article>
+                    <div className="content">
+                        {description}
+                        <hr />
+                        <nav className="level">
+                            <div className="level-left">
+                                {repoLanguage}
+                                {repoStars}
+                                {repoForks}
+                            </div>
+                            <div className="level-right">
+                                {repoStarsInPeriod}
+                                {repoUpdated}
+                            </div>
+                        </nav>
+                    </div>
+
+                </div>
+                <footer className="card-footer">
+                    <a className="card-footer-item"></a>
+                    <a className="card-footer-item" href={url}><i className="fa fa-github"></i>Open</a>
+                    <a className="card-footer-item"></a>
+                </footer>
             </div>
         )
     }
