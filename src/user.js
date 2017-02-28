9e93deb7a1c6b7d0f6abb8f83d55c16d794aacf4
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-02-28 13:44:16 +0100

    Updated user component styling

--- src/user.js
+++ src/user.js
@@ -4,19 +4,52 @@ class User extends React.Component {
     render() {
         const { img, username, name, url, bio, location, email } = this.props.user;
 
+        let userLocation;
+        let userEmail;
+
+        if (location) {
+            userLocation = <a className="level-item">
+                            <span className="tag link-tag icon is-small"><i className="fa fa-map-maker"></i>{location}</span>
+                        </a>
+        }
+
+        if (email) {
+            userEmail = <a className="level-item">
+                            <span className="tag link-tag icon is-small"><i className="fa fa-envelope-o"></i>{email}</span>
+                        </a>
+        }
+
         return (
-            <div>
-                <article>
-                    <div>
-                        <h2>{name}</h2>
-                        <p>{username}</p>
-                        <div>
-                            <span>{bio}</span>
-                            <span>{location}</span>
-                            <span>{email}</span>
+            <div className="card">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <figure className="image" height="40px" width="40px">
+                                <img src={img} alt="image" />
+                            </figure>
+                        </div>
+                        <div className="media-content">
+                            <p className="title is-4">{name}</p>
+                            <p className="subtitle is-6">@{username}</p>
                         </div>
                     </div>
-                </article>
+
+                    <div className="content">
+                        {bio}
+                        <hr />
+                        <nav className="level">
+                            <div className="level-left">
+                                {userLocation}
+                                {userEmail}
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <footer className="card-footer">
+                    <a className="card-footer-item"></a>
+                    <a className="card-footer-item" href={url}>Open</a>
+                    <a className="card-footer-item"></a>
+                </footer>
             </div>
         )
     }
