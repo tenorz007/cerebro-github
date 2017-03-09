d4cc8182e971fc6e0f00a51545dc9eb5f67cb728
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-03-09 20:51:20 +0100

    Refactored repo component
    * Added support for displaying repo readme
    * Removed uneeded variables, instead display using conditiona;s

--- src/repo.js
+++ src/repo.js
@@ -1,75 +1,86 @@
 const React = require('react');
+const ReactMarkdown = require('react-markdown');
 
 class Repo extends React.Component {
-    render() {
-        const { name, full_name, html_url, description, language, language_color, stargazers_count, stargazers_period, forks, updated } = this.props.repo;
-
-        let repoLanguage;
-        let repoStars;
-        let repoForks;
-        let repoStarsInPeriod;
-        let repoUpdated;
-
-        if (language) {
-            repoLanguage = <small className="level-item">
-                                <span className="tag link-tag icon is-small"><i className="fa fa-circle-thin" color={language_color}></i>{language}</span>
-                            </small>
-        }
-
-        if (stargazers_count) {
-            repoStars = <small className="level-item">
-                            <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stargazers_count} stars</span>
-                        </small>
-        }
-
-        if (forks) {
-            repoForks = <small className="level-item">
-                            <span className="tag link-tag icon is-small"><i className="fa fa-code-fork"></i>{forks} forks</span>
-                        </small>
-        }
-
-        if (stargazers_period) {
-            repoStarsInPeriod = <small className="level-item">
-                                    <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stargazers_period}</span>
-                                </small>
-        }
-
-        if (updated) {
-            repoUpdated = <small className="level-item">
-                                <span className="tag link-tag icon is-small"><i className="fa fa-pencil-square-o"></i>{updated}</span>
-                            </small>
-        }
+    renderReadMe() {
+        const { readme } = this.props;
+        const body = new Buffer(readme.content, readme.encoding).toString('ascii');
 
         return (
-            <div className="card">
+            <div className="card-content">
+                <div className="content">
+                    <ReactMarkdown source={body} />
+                </div>
+            </div>
+        );
+    }
+
+    renderBody() {
+        const { repo, readme, goBack, onClick } = this.props;
+        const { name, full_name, html_url, description, language,
+            language_color, stargazers_count, stargazers_period,
+            forks, updated
+        } = repo;
+
+        return (
+            <div className="card" tabIndex='1'>
                 <div className="card-content">
+
                     <div className="media-content">
                         <p className="title is-5"><i className="fa fa-storage"></i><strong>{full_name}</strong></p>
                     </div>
+
                     <div className="content">
                         {description}
                         <hr />
                         <nav className="level">
                             <div className="level-left">
-                                {repoLanguage}
-                                {repoStars}
-                                {repoForks}
+                                {language ? (
+                                    <small className="level-item">
+                                        <span className="tag link-tag icon is-small"><i className="fa fa-circle-thin" color={language_color}></i>{language}</span>
+                                    </small>
+                                ) : ('') }
+                                {stargazers_count ? (
+                                    <small className="level-item">
+                                        <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stargazers_count} stars</span>
+                                    </small>
+                                ) : ('')}
+                                {forks ? (
+                                    <small className="level-item">
+                                        <span className="tag link-tag icon is-small"><i className="fa fa-code-fork"></i>{forks} forks</span>
+                                    </small>
+                                ) : ('')}
                             </div>
                             <div className="level-right">
-                                {repoStarsInPeriod}
-                                {repoUpdated}
+                                {stargazers_period ? (
+                                    <small className="level-item">
+                                        <span className="tag link-tag icon is-small"><i className="fa fa-star-o"></i>{stargazers_period}</span>
+                                    </small>
+                                ) : ('')}
+                                {updated ? (
+                                    <small className="level-item">
+                                        <span className="tag link-tag icon is-small"><i className="fa fa-pencil-square-o"></i>{updated}</span>
+                                    </small>
+                                ) : ('')}
                             </div>
                         </nav>
                     </div>
 
+                    <footer className="card-footer">
+                        {readme
+                            ? (<a className="card-footer-item" onClick={goBack}><i className="fa fa-chevron-circle-left"></i>Back</a>)
+                            : (<a className="card-footer-item" onClick={onClick}><i className="fa fa-book"></i>View</a>)
+                        }
+                        <a className="card-footer-item" href={html_url}><i className="fa fa-github"></i>Open</a>
+                    </footer>
+                    {readme ? (this.renderReadMe()) : ('')}
                 </div>
-                <footer className="card-footer">
-                    <a className="card-footer-item"></a>
-                    <a className="card-footer-item" href={html_url}><i className="fa fa-github"></i>Open</a>
-                    <a className="card-footer-item"></a>
-                </footer>
             </div>
-        )
+        );
+    }
+
+    render() {
+        return this.renderBody();
     }
 }
 
